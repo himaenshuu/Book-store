f79bce73446e1fdb72d4275d2951eb5655217f12
him <118502619+Himaenshuuu@users.noreply.github.com>
2025-01-12 18:51:33 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get("http://localhost:5000/books") //
+      .get("https://book-store-backend-z64m.onrender.com/books") //
       .then((response) => {
         setBooks(response.data);
         setLoading(false);
