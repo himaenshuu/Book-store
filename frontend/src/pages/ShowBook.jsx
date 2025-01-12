2d04fbc55641794c97587b3979705b58e87f2eb5
him <118502619+Himaenshuuu@users.noreply.github.com>
2025-01-12 18:52:07 +0530

    Update ShowBook.jsx

--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -12,7 +12,7 @@ const ShowBook = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get(`http://localhost:5000/books/${id}`)
+      .get(`https://book-store-backend-z64m.onrender.com/books/${id}`)
       .then((response) => {
         setBook(response.data);
         setLoading(false);
